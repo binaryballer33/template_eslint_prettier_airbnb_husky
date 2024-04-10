d0e3792efeb95830c6af4feac6c10232531c04f9
Shaquille Mandy <shaqmandy@gmail.com>
2024-04-10 16:13:06 -0700

    got prettier-plugin-tailwindcss working for className sorting

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "../../../../../../../vite.svg"
-import "./App.css"
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button type="button" onClick={() => setCount((the_count) => the_count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <h1 className="m-4 min-h-dvh bg-black p-4 text-center text-white">Hello World</h1>{" "}
+      <button
+        type="button"
+        className="m-4 mt-2 bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3"
+      >
+        Test Tailwind Button
+      </button>
     </>
   )
 }
